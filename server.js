08d6c7d09adcd54669b0cbb33a9dff54fadf0e5a
Psophonthamm <Panithan.Sophonthammakhun@agoda.com>
2020-04-26 19:40:43 +0700

    create mongoDB connection

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
 const morgan = require('morgan')
+const connectDB = require('./config/db')
 
 dotenv.config({
     path: './config/config.env'
 })
+
+connectDB()
 
 // routers
 const transactions = require('./routes/transactions')
