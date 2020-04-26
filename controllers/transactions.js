91945a5b2a8e2a81f3142bd28e13ccf0dc5ded77
Psophonthamm <Panithan.Sophonthammakhun@agoda.com>
2020-04-26 19:19:23 +0700

    add and delete transactions

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -4,3 +4,17 @@
 exports.getTransactions = (req, res, next) => {
     res.send('GET transactions')
 }
+
+// @desc Add a transaction
+// @route POST /api/v1/transactions
+// @access Public
+exports.addTransactions = (req, res, next) => {
+    res.send('POST transaction')
+}
+
+// @desc Delete a transaction
+// @route DELETE /api/v1/transactions/:id
+// @access Public
+exports.deleteTransaction = (req, res, next) => {
+    res.send('DELETE transaction')
+}
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router();
-const { getTransactions } = require('../controllers/transactions')
+const { getTransactions, addTransactions, deleteTransaction } = require('../controllers/transactions')
 
 router
     .route('/')
     .get(getTransactions)
+    .post(addTransactions)
+
+router
+    .route('/:id')
+    .delete(deleteTransaction)
 
 module.exports = router;
