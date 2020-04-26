b4c93fafcf4ada5ad38f6edf1a770d159758e37f
Psophonthamm <Panithan.Sophonthammakhun@agoda.com>
2020-04-26 19:45:54 +0700

    impl get transactions

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,20 +1,34 @@
+const Transaction = require('../models/Transaction')
+
 // @desc Get all transactions
 // @route GET /api/v1/transactions
 // @access Public
-exports.getTransactions = (req, res, next) => {
-    res.send('GET transactions')
+exports.getTransactions = async (req, res, next) => {
+    try {
+        const transactions = await Transaction.find()
+        return res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+        })
+    } catch (error) {
+        return res.send(500).json({
+            success: false,
+            message: `Server error: ${error.message}`
+        })
+    }
 }
 
 // @desc Add a transaction
 // @route POST /api/v1/transactions
 // @access Public
-exports.addTransactions = (req, res, next) => {
+exports.addTransactions = async (req, res, next) => {
     res.send('POST transaction')
 }
 
 // @desc Delete a transaction
 // @route DELETE /api/v1/transactions/:id
 // @access Public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
     res.send('DELETE transaction')
 }
